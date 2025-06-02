55b6b4286d8e5c8a8a62af300825917ad9224b55
ericyusmile <57234182+ericyusmile@users.noreply.github.com>
2025-06-02 14:06:05 -0500

    linking_matrix_zero

--- helpers.py
+++ helpers.py
@@ -1,2 +1,11 @@
 def sig_zero(L):
     return (L.signature() == 0)
+
+def linking_matrix_zero(L):
+    output = True
+    M = L.linking_matrix()
+    for i in M:
+        for j in i:
+            if (j != 0):
+                output = False
+    return output
