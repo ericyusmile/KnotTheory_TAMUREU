15872fec956cd62c40b21d4eac2c7c408aa6085d
ericyusmile <57234182+ericyusmile@users.noreply.github.com>
2025-06-03 00:53:00 -0500

    flawed eisermann

--- helpers.py
+++ helpers.py
@@ -9,3 +9,21 @@ def linking_matrix_zero(L):
             if (j != 0):
                 output = False
     return output
+
+def eisermann(L):
+    n = len(L.link_components)
+    jones_L = L.jones_polynomial()
+    jones_unlink = (-(q + q^(-1))^(n-1))
+    if not jones_unlink.divides(jones_L):
+        return false
+    quotient = jones_L / jones_unlink
+    components = L.link_components
+    determinant_product = 1
+    for component in components:
+        K = L.sublink(component)
+        if (len(K.link_components) != 0):
+            determinant_product *= K.determinant()
+    quotientI = quotient(I)
+    if (quotientI.imag_part() == 0 and (quotientI.real() % 32 == determinant_product % 32 or int(quotientI.real()) % 32 == -determinant_product % 32)):
+        return True
+    return False
